7ecc1f84e660bcc0669174317311043edb65a1df
bbarba <bbarba9524@gmail.com>
2015-10-05 18:52:49 -0700

    Added files

--- src/wordDatabase.java
+++ src/wordDatabase.java
@@ -1,3 +1,16 @@
+/**
+ * Hangman
+ * 
+ * Group 3
+ * 
+ * A Hangman game that uses a random word generator from categories. 
+ * It determines which player wins based on best out of two/three. 
+ * It will allow a certain amount of moves until the player loses.
+ * 
+ * @author Brandon Barba (bbarba2@asu.edu)
+ * @author Vianelis Martinez (Vianelis.Martinez@asu.edu)
+ * @author Jeff Thomas (Jeff.Thomas.1@asu.edu)
+*/
 
 public class wordDatabase {
 
