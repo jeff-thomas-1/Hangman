4e7cd58dc2290333dfc24c8c8c852a595c057c8c
jeff-thomas-1 <jeff.thomas.1@asu.edu>
2015-10-07 21:58:43 -0700

    Create hardWord.java

---
+++ src/hardWord.java
@@ -0,0 +1,30 @@
+public class hardWord {
+
+	private ArrayList <String> wordList = new ArrayList <String> ();	
+
+	public hardWord() {
+	//adds the individual words in the file to the array list
+		try {
+			BufferedReader hangmanWords = new BufferedReader(new FileReader("hardWord.txt"));
+			while(true) {
+				String line = hangmanWords.readLine();
+				if(line == null) break;
+				wordList.add(line);
+			}
+			hangmanWords.close();
+		} catch (IOException ex) {
+			 
+		}
+	}
+
+/** Returns the word at the specified index. */
+	public String getWord(int index) {
+		return wordList.get(index);
+
+	}
+
+	
+	/** Returns the number of words in the caagory selected */
+	public int getWordCount() {
+		return wordList.size();
+	}	
