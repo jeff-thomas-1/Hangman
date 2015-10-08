f6de5bb943e0cdafb64e1d1bdd530532f2ff8bc8
jeff-thomas-1 <jeff.thomas.1@asu.edu>
2015-10-07 21:55:10 -0700

    Update wordDatabase.java

--- src/wordDatabase.java
+++ src/wordDatabase.java
@@ -12,6 +12,35 @@
  * @author Jeff Thomas (Jeff.Thomas.1@asu.edu)
 */
 
-public class wordDatabase {
 
+public class easyWord {
+
+	private ArrayList <String> wordList = new ArrayList <String> ();	
+
+	public easyWord() {
+	//adds the individual words in the file to the array list
+		try {
+			BufferedReader hangmanWords = new BufferedReader(new FileReader("EasyWord.txt"));
+			while(true) {
+				String line = hangmanWords.readLine();
+				if(line == null) break;
+				wordList.add(line);
+			}
+			hangmanWords.close();
+		} catch (IOException ex) {
+			 
+		}
+	}
+
+/** Returns the word at the specified index. */
+	public String getWord(int index) {
+		return wordList.get(index);
+
+	}
+
+	
+	/** Returns the number of words in the caagory selected */
+	public int getWordCount() {
+		return wordList.size();
+	}	
 }
